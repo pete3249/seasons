72a2bfbf6e5ce72b66553925a8963f2112c70ac2
pete3249 <katelynrpeterson16@gmail.com>
2021-04-20 14:55:28 -0400

    Remove comments

--- src/index.js
+++ src/index.js
@@ -4,28 +4,11 @@ import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner'
 
 class App extends React.Component {
-    // App component is borrowing functionality from React.Component class
-    // When we define constructor function in our class, we are overriding constructor function of React.Component
-    // To make sure Parent function gets called, call super(props), which is reference to Parent constructor function
-    // constructor(props) {
-        // super(props);
-        // THIS IS THE ONLY TIME WE DIRECTLY ASSIGN STATE
-        // this.state = { lat: null, errorMessage: '' };
-        // initializing property inside of state object
-        // null means we do not know yet what the latitude is
-    //}
-
-    // the abbreviated sytax below is doing the exact same thing as the constructor function
-    // initialized as an instance property (babel moves it back into constructor)
     state = { lat: null, errorMessage: ''}
     
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            // success callback, will not be invoked until after location is returned (some point in the future)
-            // to update state object, called setState()
             position => this.setState({lat : position.coords.latitude}),
-                // we did not write this.state.lat = position.coords.latitude
-                // NEVER WRITE ABOVE
             err => this.setState({errorMessage: err.message})
         );
     }
